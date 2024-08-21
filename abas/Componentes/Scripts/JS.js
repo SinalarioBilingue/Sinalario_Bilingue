0487c381566332445ee260d5c3b3436e80454af5
SinalarioBilingue <sinalariobilingue@gmail.com>
2024-08-20 22:45:41 -0300

    mudou

--- abas/Componentes/Scripts/JS.js
+++ abas/Componentes/Scripts/JS.js
@@ -27,16 +27,3 @@ window.onload = function() {
     showPage(currentPage);
 }
 
-
-window.addEventListener('scroll', function() {
-    let scroll = document.querySelector('.scrollTop');
-    scroll.classList.toggle('active', window.scrollY > 450);
-});
-
-function backTop() {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
-}
-
